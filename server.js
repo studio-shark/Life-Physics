b8f9461fc1fffedbe293c2f0a143d42812d44181
Shark Studio <84243246+studio-shark@users.noreply.github.com>
2026-02-21 22:43:26 +0200

    feat: Bind server to all interfaces
    Bind the Express server to '0.0.0.0' to allow access from external network interfaces, in addition to localhost. This is necessary for deployment scenarios where the server needs to be reachable from outside the host machine.

--- server.js
+++ server.js
@@ -359,6 +359,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
